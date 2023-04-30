6236f3ddc2d31c2e62256ecb8db7edbeea8d0b03
Devin Miller <devook@gmail.com>
2023-04-29 21:58:29 -0700

    Add in FMOD integration and banks

---
+++ LudumDare53/Assets/Scripts/BeeBuzzSound.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+namespace LeftOut.LudumDare
+{
+    public class BeeBuzzSound : MonoBehaviour
+    {
+        public FMODUnity.StudioEventEmitter buzz;
+
+        // buzz intensity parameter
+        public float buzzSound = 10;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            buzz = GetComponent<FMODUnity.StudioEventEmitter>();
+        }
+
+        private void Update()
+        {
+            buzz.EventInstance.setParameterByName("Buzz", buzzSound);
+        }
+
+    }
+}
