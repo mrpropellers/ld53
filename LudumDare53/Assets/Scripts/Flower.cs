15830be2524020adb3cb14c477cef6c7a3773792
Devin Miller <devook@gmail.com>
2023-04-29 17:44:02 -0700

    Add Pollen to Flowers

--- LudumDare53/Assets/Scripts/Flower.cs
+++ LudumDare53/Assets/Scripts/Flower.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace LeftOut.LudumDare
@@ -7,19 +5,33 @@ namespace LeftOut.LudumDare
     public class Flower : MonoBehaviour
     {
         [SerializeField]
+        Color PollenColor;
+        
+        [SerializeField]
         Transform FlowerCenter;
-
+        
         public Transform LandingPointCenter => FlowerCenter;
         // Start is called before the first frame update
         void Start()
         {
         
         }
-
+        
         // Update is called once per frame
         void Update()
         {
         
+        }
+
+        public void ReceivePollen(Pollen incomingPollen)
+        {
+            Debug.Log($"{incomingPollen.Color} received.");
+        }
+
+        public Pollen GivePollen()
+        {
+            Debug.Log($"Giving some {PollenColor} pollen");
+            return new Pollen(PollenColor);
         }
     }
 }
---
+++ LudumDare53/Assets/Scripts/Pollen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace LeftOut.LudumDare
+{
+    public struct Pollen
+    {
+        public Color Color { get; private set; }
+
+        public Pollen(Color color)
+        {
+            Color = color;
+        }
+    }
+}
