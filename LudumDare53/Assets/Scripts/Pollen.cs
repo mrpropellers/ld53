f0246ebe336d656458a7b1cb5d2115e1a5416443
Amanda Trang <amanda.trang@unity3d.com>
2023-04-30 23:24:09 -0700

    Minor fix

--- LudumDare53/Assets/Scripts/Pollen.cs
+++ LudumDare53/Assets/Scripts/Pollen.cs
@@ -8,7 +8,14 @@ namespace LeftOut.LudumDare
     [Serializable]
     public class Pollen
     {
+        public enum ColorNames { Purple, Pink, Red, Orange, Yellow, White };
         public static string[] HexList = { "#AC92EC", "#EC87C0", "#ED5565", "#FC6E51", "#FFCE54", "#F5F7FA" };
+
+        public Dictionary<ColorNames, string> ColorNameToHex = new Dictionary<ColorNames, string>()
+        {
+            {ColorNames.Purple, "#AC92EC"}, {ColorNames.Pink, "#EC87C0"}, {ColorNames.Red, "#ED5565"}, {ColorNames.Orange, "#FC6E51"},
+            {ColorNames.Yellow, "#FFCE54"}, {ColorNames.White, "#F5F7FA"}
+        };
         public Flower Parent { get; set; }
         public Color Color { get; private set; }
 
